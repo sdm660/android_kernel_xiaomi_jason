2ef992674f09bd9f4407fb2d7ff4c3be629d0283
celtare21 <celtare21@gmail.com>
2019-07-31 18:44:46 +0000

    sched: fair: Remove double_(un)lock_balance fuckery from CAF
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8158,13 +8158,7 @@ static void detach_task(struct task_struct *p, struct lb_env *env)
 
 	p->on_rq = TASK_ON_RQ_MIGRATING;
         deactivate_task(env->src_rq, p, DEQUEUE_NOCLOCK);
-#ifdef CONFIG_SCHED_WALT
-	double_lock_balance(env->src_rq, env->dst_rq);
-#endif
 	set_task_cpu(p, env->dst_cpu);
-#ifdef CONFIG_SCHED_WALT
-	double_unlock_balance(env->src_rq, env->dst_rq);
-#endif
 }
 
 /*
